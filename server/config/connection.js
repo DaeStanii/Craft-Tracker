d971cce00382ab287e37d9ced38249bb64a105c1
Dae'Stanii Spackman <daestanii1997@gmail.com>
2024-04-16 15:43:31 -0600

    updated mongodb connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/craft-tracker');
+mongoose.connect(process.env.MONGODB_URI2 || 'mongodb+srv://daestanii1997:NikitaKai@crafttracker.cosigbq.mongodb.net/?retryWrites=true&w=majority&appName=CraftTracker');
 
 module.exports = mongoose.connection;
