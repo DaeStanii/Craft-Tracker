9f657f9dbfa5984b4b93e996dc2d8c0e90886c80
Dae'Stanii Spackman <daestanii1997@gmail.com>
2024-04-16 14:40:56 -0600

    updated server file

--- server/server.js
+++ server/server.js
@@ -33,7 +33,8 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    // app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use("/", express.static("../client/dist"));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
